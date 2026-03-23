91ae37b974dac7d5f77e2a25cd7d5969d1c9938a
Dzmitry Prymudrau <dprymudrau@gmail.com>
2026-03-23 13:55:20 +0400

    initial commit

---
+++ src/main/java/com/solvd/demo/pages/HomePage.java
@@ -0,0 +1,10 @@
+package com.solvd.demo.pages;
+
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends AbstractPage {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+}
