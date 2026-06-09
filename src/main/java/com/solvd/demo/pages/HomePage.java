5450bfaff18da226294ca81448d2c3cd09adf044
Dzmitry Prymudrau <dprymudrau@gmail.com>
2026-06-09 13:24:30 +0400

    Add automated tests for PLTV2-1339

--- src/main/java/com/solvd/demo/pages/HomePage.java
+++ src/main/java/com/solvd/demo/pages/HomePage.java
@@ -1,10 +1,29 @@
 package com.solvd.demo.pages;
 
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.gui.AbstractPage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends AbstractPage {
+
+    public static final String HOME_PAGE_URL = "https://www.underarmour.com";
+
+    @FindBy(id = "truste-consent-button")
+    private ExtendedWebElement acceptCookiesButton;
+
     public HomePage(WebDriver driver) {
         super(driver);
+        setPageAbsoluteURL(HOME_PAGE_URL);
+    }
+
+    public ExtendedWebElement getAcceptCookiesButton() {
+        return acceptCookiesButton;
+    }
+
+    public void acceptCookies() {
+        if (acceptCookiesButton.isPresent(10)) {
+            acceptCookiesButton.click();
+        }
     }
 }
---
+++ src/main/java/com/solvd/demo/pages/LoginPage.java
@@ -0,0 +1,114 @@
+package com.solvd.demo.pages;
+
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends AbstractPage {
+
+    public static final String LOGIN_PAGE_URL = "https://www.underarmour.com/en-us/login";
+
+    @FindBy(xpath = "//h1//img[@alt='us-web logo']")
+    private ExtendedWebElement uaLogo;
+
+    @FindBy(xpath = "//h2[normalize-space()='Log In']")
+    private ExtendedWebElement logInHeading;
+
+    @FindBy(xpath = "//input[@id='identifier']")
+    private ExtendedWebElement emailAddressInput;
+
+    @FindBy(xpath = "//input[@id='credentials.passcode']")
+    private ExtendedWebElement passwordInput;
+
+    @FindBy(xpath = "//button[@aria-label='Show password']")
+    private ExtendedWebElement showPasswordToggle;
+
+    @FindBy(xpath = "//label[@id='rememberMe-checkbox']")
+    private ExtendedWebElement keepMeLoggedInCheckbox;
+
+    @FindBy(xpath = "//button[normalize-space()='Forgot Password?']")
+    private ExtendedWebElement forgotPasswordLink;
+
+    @FindBy(xpath = "//button[@type='submit' and normalize-space()='Log In']")
+    private ExtendedWebElement logInButton;
+
+    @FindBy(xpath = "//button[normalize-space()='Continue as a Guest']")
+    private ExtendedWebElement continueAsGuestButton;
+
+    @FindBy(xpath = "//p[contains(.,'New to Under Armour?')]")
+    private ExtendedWebElement newToUnderArmourText;
+
+    @FindBy(xpath = "//button[normalize-space()='Create Account']")
+    private ExtendedWebElement createAccountLink;
+
+    @FindBy(xpath = "//a[normalize-space()=\"UA Rewards' Terms & Conditions\"]")
+    private ExtendedWebElement uaRewardsTermsLink;
+
+    @FindBy(xpath = "//a[normalize-space()='Privacy Policy']")
+    private ExtendedWebElement privacyPolicyLink;
+
+    @FindBy(xpath = "//a[normalize-space()='Terms & Conditions']")
+    private ExtendedWebElement termsAndConditionsLink;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        setPageAbsoluteURL(LOGIN_PAGE_URL);
+    }
+
+    public ExtendedWebElement getUaLogo() {
+        return uaLogo;
+    }
+
+    public ExtendedWebElement getLogInHeading() {
+        return logInHeading;
+    }
+
+    public ExtendedWebElement getEmailAddressInput() {
+        return emailAddressInput;
+    }
+
+    public ExtendedWebElement getPasswordInput() {
+        return passwordInput;
+    }
+
+    public ExtendedWebElement getShowPasswordToggle() {
+        return showPasswordToggle;
+    }
+
+    public ExtendedWebElement getKeepMeLoggedInCheckbox() {
+        return keepMeLoggedInCheckbox;
+    }
+
+    public ExtendedWebElement getForgotPasswordLink() {
+        return forgotPasswordLink;
+    }
+
+    public ExtendedWebElement getLogInButton() {
+        return logInButton;
+    }
+
+    public ExtendedWebElement getContinueAsGuestButton() {
+        return continueAsGuestButton;
+    }
+
+    public ExtendedWebElement getNewToUnderArmourText() {
+        return newToUnderArmourText;
+    }
+
+    public ExtendedWebElement getCreateAccountLink() {
+        return createAccountLink;
+    }
+
+    public ExtendedWebElement getUaRewardsTermsLink() {
+        return uaRewardsTermsLink;
+    }
+
+    public ExtendedWebElement getPrivacyPolicyLink() {
+        return privacyPolicyLink;
+    }
+
+    public ExtendedWebElement getTermsAndConditionsLink() {
+        return termsAndConditionsLink;
+    }
+}
